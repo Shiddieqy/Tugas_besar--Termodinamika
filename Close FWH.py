91d7de84186b784ffe8ff8d28a8164301ae0ebf7
Shabri Ash Shiddieqy <shabriashiddieqy@gmail.com>
2021-12-19 14:35:42 +0700

    update output

--- Close FWH.py
+++ Close FWH.py
@@ -23,7 +23,7 @@ y = 0.5
 
 while (True):
     Pu2 = Pu/5
-    h2s = CP.PropsSI('H', 'P', Pu2, 'S', S1, 'Water')
+    h2s = CP.PropsSI('H', 'P', Pu, 'S', S1, 'Water')
     h2 = 1.54*h2s-0.54*h1
     h3 = CP.PropsSI('H', 'P', Pu2, 'Q', 0, 'Water')
     S3 = CP.PropsSI('S', 'P', Pu2, 'Q', 0, 'Water')
@@ -34,7 +34,7 @@ while (True):
     h7 = 0.9*h7s+0.1*(0.1*h5+0.9*h6s)
     h6 = 0.1*h5+0.9*h6s
     T = CP.PropsSI('T', 'P', Pu , 'H', h5, 'Water')
-    y = (h3-h2)/(h6-h2)
+    y = (h4-h2)/(h6-h3+h4-h2)
     Pu += 10000
 
     print(Pu,T,((h6-h3)-(1-y)*(h6-h7-h2+h1))/(h5-h4))
@@ -42,9 +42,10 @@ while (True):
         break
 print("T    H   S   P   X")
 print("1", CP.PropsSI('T', 'P', Pb, 'Q', 0, 'Water'), h1, S1, Pb, "0")
-print("2", CP.PropsSI('T', 'P', Pu2, 'H', h2, 'Water'), h2, CP.PropsSI('S', 'P', Pu2, 'H', h2, 'Water'), Pu2, "Liquid")
+print("2", CP.PropsSI('T', 'P', Pu, 'H', h2, 'Water'), h2, CP.PropsSI('S', 'P', Pu, 'H', h2, 'Water'), Pu, "Liquid")
 print("3", CP.PropsSI('T', 'P', Pu2, 'H', h3, 'Water'), h3, CP.PropsSI('S', 'P', Pu2, 'H', h3, 'Water'), Pu2, CP.PropsSI('Q', 'P', Pu2, 'H', h3, 'Water'))
 print("4", CP.PropsSI('T', 'P', Pu, 'H', h4, 'Water'), h4, CP.PropsSI('S', 'P', Pu, 'H', h4, 'Water'), Pu, CP.PropsSI('Q', 'P', Pu, 'H', h4, 'Water'))
 print("5", T, h5, CP.PropsSI('S', 'P', Pu, 'H', h5, 'Water'), Pu, CP.PropsSI('Q', 'P', Pu, 'H', h5, 'Water'))
 print("6", CP.PropsSI('T', 'P', Pu2, 'H', h6, 'Water'), h6, CP.PropsSI('S', 'P', Pu2, 'H', h6, 'Water'), Pu2, CP.PropsSI('Q', 'P', Pu2, 'H', h6, 'Water'))
 print("7", CP.PropsSI('T', 'P', Pb, 'H', h7, 'Water'), h7, CP.PropsSI('S', 'P', Pb, 'H', h7, 'Water'), Pb, CP.PropsSI('Q', 'P', Pb, 'H', h7, 'Water'))
+
--- Reheat.py
+++ Reheat.py
@@ -34,4 +34,12 @@ while (True):
     if ((h6-h1)/(h3+h5-h2-h4) <= 0.55):
         break
 
-print(Pu)
+print("T    H   S   P   X")
+print("1", CP.PropsSI('T', 'P', Pb, 'Q', 0, 'Water'), h1, S1, Pb, "0")
+print("2", CP.PropsSI('T', 'P', Pu, 'H', h2, 'Water'), h2, CP.PropsSI('S', 'P', Pu, 'H', h2, 'Water'), Pu, "Liquid")
+print("3", T, h3, CP.PropsSI('S', 'P', Pu, 'H', h3, 'Water'), Pu, "Super Heated")
+print("4", CP.PropsSI('T', 'P', Pu2, 'H', h4, 'Water'), h4, CP.PropsSI('S', 'P', Pu2, 'H', h4, 'Water'), Pu2, CP.PropsSI('Q', 'P', Pu2, 'H', h4, 'Water'))
+print("5", T, h5, CP.PropsSI('S', 'P', Pu2, 'H', h5, 'Water'), Pu2, CP.PropsSI('Q', 'P', Pu2, 'H', h5, 'Water'))
+print("6", CP.PropsSI('T', 'P', Pb, 'H', h6, 'Water'), h6, CP.PropsSI('S', 'P', Pb, 'H', h6, 'Water'), Pb, CP.PropsSI('Q', 'P', Pb, 'H', h6, 'Water'))
+
+
